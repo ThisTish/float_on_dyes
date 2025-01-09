73c407c745f38b4a67a0b58c72a222f7726b8ff1
Tish <tishthegoat@gmail.com>
2025-01-08 19:33:58 -0700

    initial commit, plans, and color test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        darkGreen: "var(--darkGreen)",
+        lightGreen: "var(--lightGreen)",
+        darkBlue: "var(--darkBlue)",
+        lightBlue: "var(--lightBlue)",
+        brightBlue: "var(--brightBlue)",
+        lightCta: "var(--lightCta)",
+        darkCta: "var(--darkCta)",
+
       },
     },
   },
