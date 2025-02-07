feb273677c8c49e157cd153b2cc06421a9bc6ac3
Tish <tishthegoat@gmail.com>
2025-02-06 20:00:19 -0700

    loader animation fix

--- app/development/(root)/page.tsx
+++ app/development/(root)/page.tsx
@@ -1,9 +1,12 @@
 import FeaturedDiscs from "@/components/landing/FeaturedDiscs"
 import Hero from "@/components/landing/Hero"
 
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms))
 
 
 const HomePage = async () => {
+
+  await delay(3000)
 
   return (
     <div>
