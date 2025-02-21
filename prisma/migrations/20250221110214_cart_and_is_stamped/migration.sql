12ec44d46d24279dce6ce8da2e01b0c5ff6e4a02
Tish <tishthegoat@gmail.com>
2025-02-21 04:05:14 -0700

    cart validators/types/schema

---
+++ prisma/migrations/20250221110214_cart_and_is_stamped/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "isStamped" BOOLEAN NOT NULL DEFAULT true;
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID,
+    "sessionCartId" TEXT NOT NULL,
+    "items" JSON[] DEFAULT ARRAY[]::JSON[],
+    "itemsPrice" DECIMAL(12,2) NOT NULL DEFAULT 0,
+    "totalPrice" DECIMAL(12,2) NOT NULL DEFAULT 0,
+    "shippingPrice" DECIMAL(12,2) NOT NULL DEFAULT 0,
+    "taxPrice" DECIMAL(12,2) NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,10 @@
-import { insertProductSchema } from "@/lib/validators"
+import { cartItemSchema, insertCartSchema, insertProductSchema } from "@/lib/validators"
 import { z } from "zod"
 
 export type Product = z.infer<typeof insertProductSchema> & {
 	id: string,
 	createdAt: Date
 }
+
+export type Cart = z.infer<typeof insertCartSchema>
+export type CartItem = z.infer<typeof cartItemSchema>
