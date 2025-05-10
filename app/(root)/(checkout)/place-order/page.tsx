00574d7bc5a94b23c8244e032ac1eb38799c6702
Tish <tishthegoat@gmail.com>
2025-05-10 05:25:54 -0600

    place order layout bug

--- app/(root)/(checkout)/place-order/page.tsx
+++ app/(root)/(checkout)/place-order/page.tsx
@@ -15,10 +15,11 @@ const PlaceOrderPage = async () => {
 
 	return (
 		<div>
-			<Heading first="Place " second="Order" className="mb-10" />
-			<div className="grid grid-rows-4 space-y-5 md:grid-cols-3 md:grid-rows-2 md:gap-5 md:space-y-0">
+			<Heading first="Place " second="Order" className="mb-10 justify-center text-darkBlue" />
+			<div className="flex flex-col space-y-5 md:grid md:grid-cols-3 md:grid-rows-2 md:gap-5 md:space-y-0">
+
 				{/* items table */}
-				<Card className="overflow-x-auto p-5 md:col-span-2">
+				<Card className="h-fit overflow-x-auto p-5 md:col-span-2 md:row-span-2">
 					<CardHeader>
 						<CardTitle className="text-xl font-semibold">
 							<h2 >Order Items</h2>
@@ -29,28 +30,28 @@ const PlaceOrderPage = async () => {
 						<ItemsTable showOptions={false} />
 					</CardContent>
 				</Card>
-
-				{/* price breakdown */}
-				<Card className="h-fit p-5 md:col-span-1 md:col-start-1">
-					<CardHeader>
-						<CardTitle className="px-0 text-xl font-semibold">
-							<h2>Order Total</h2>
-						</CardTitle>
-					</CardHeader>
-					<CardContent className="space-y-5">
-						<PriceBreakdown />
-						<PlaceOrderForm />
-					</CardContent>
-				</Card>
-
-				<div className="row-start-2 space-y-5 md:col-span-1 md:row-start-1">
+				<div className="space-y-5 md:col-span-1 md:row-start-1">
 					{/* shipping address */}
 					<AddressCard />
 
 					{/* payment method */}
 					<PaymentCard />
+					{/* price breakdown */}
+					<Card className="h-fit p-5 md:col-span-1 md:col-start-1">
+						<CardHeader>
+							<CardTitle className="px-0 text-xl font-semibold">
+								<h2>Order Total</h2>
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="space-y-5">
+							<PriceBreakdown />
+							<PlaceOrderForm />
+						</CardContent>
+					</Card>
 				</div>
 			</div>
+
+
 		</div>
 	)
 }
