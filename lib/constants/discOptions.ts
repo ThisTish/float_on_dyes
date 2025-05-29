868f9dfa4eff55d297019b15b23438df1d77b8fa
Tish <tishthegoat@gmail.com>
2025-05-29 05:53:31 -0600

    brand logo work-images & constant set up

--- lib/constants/discOptions.ts
+++ lib/constants/discOptions.ts
@@ -175,3 +175,82 @@ export const dyeTypes = [
 
 export const RIMOPTIONS = ['Rim Spin', 'Rim Dip', 'Full Back Dip'] as const
 export const STAMPOPTIONS = ['Glue Mask', 'Wipe Stamp'] as const
+
+export const BRANDS = [
+	{
+		name: 'Axiom',
+		image: '/images/logos/Axiom.svg',
+	},
+	{
+		name: 'Clash',
+		image: '/images/logos/Clash.svg',
+	},
+	{
+		name: 'DGA',
+		image: '/images/logos/DGA.svg',
+	},
+	{
+		name: 'Discmania',
+		image: '/images/logos/Discmania.svg',
+	},
+	{
+		name: 'Discraft',
+		image: '/images/logos/Discraft.svg',
+	},
+	{
+		name: 'Dynamic Discs',
+		image: '/images/logos/DynamicDiscs.svg',
+	},
+	{
+		name: 'Handeye',
+		image: '/images/logos/Handeye.svg',
+	},
+	{
+		name: 'Infinite',
+		image: '/images/logos/Infinite.svg',
+	},
+	{
+		name: 'Innova',
+		image: '/images/logos/Innova.svg',
+	},
+	{
+		name: 'Kastaplast',
+		image: '/images/logos/Kastaplast.svg',
+	},
+	{
+		name: 'Latitude 64',
+		image: '/images/logos/Latitude64.svg',
+	},
+	{
+		name: 'Lone Star',
+		image: '/images/logos/LoneStar.svg',
+	},
+	{
+		name: 'Mint',
+		image: '/images/logos/Mint.svg',
+	},
+	{
+		name: 'MVP',
+		image: '/images/logos/MVP.svg',
+	},
+	{
+		name: 'Streamline',
+		image: '/images/logos/Streamline.svg',
+	},
+	// {
+	// 	name: 'Prodigy',
+	// 	image: '/images/logos/Prodigy.svg',
+	// },
+	{
+		name: 'Thought Space',
+		image: '/images/logos/Thought-Space.svg',
+	},
+	{
+		name: 'Trash Panda',
+		image: '/images/logos/TrashPanda.svg',
+	},
+	{
+		name: 'Westside',
+		image: '/images/logos/Westside.svg',
+	}
+]
