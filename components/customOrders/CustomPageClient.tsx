16b13057ecd3419f97b72b2fec7d221842b0edfd
Tish <tishthegoat@gmail.com>
2025-06-05 04:53:15 -0600

    contact area on custom page

--- components/customOrders/CustomPageClient.tsx
+++ components/customOrders/CustomPageClient.tsx
@@ -3,9 +3,12 @@
 import { Decimal } from "@prisma/client/runtime/library"
 import { useCallback, useState } from "react"
 import CustomDyeImages from "./CustomDyeImages"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
 import CustomOrderForm from "./CustomOrderForm"
 import Link from "next/link"
+import { Button } from "../ui/button"
+import { AnimatedDiv } from "../ui/AnimatedDiv"
+import { Send } from "lucide-react"
 
 type CustomOrderFormProps = {
 	discs: {
@@ -38,19 +41,38 @@ const CustomPageClient = ({ discs }: CustomOrderFormProps) => {
 			</div>
 
 			{/* form */}
-			<div>
-				<Card className="p-5">
-					<CardHeader>
-						<CardTitle className="px-0 text-2xl">Custom Order Form</CardTitle>
-						<CardDescription className="-mt-2 text-pretty px-0">Please select from the options below or<Link href="/contact" className="text-brightBlue hover:text-primary" > contact us</Link> for more options or complex requests.</CardDescription>
-					</CardHeader>
+			<Card className="p-5">
+				<CardHeader>
+					<CardTitle className="px-0 text-2xl">Custom Order Form</CardTitle>
+					<CardDescription className="-mt-2 text-pretty px-0">Please select from the options below or<Link href="/contact" className="font-bold text-darkGreen hover:text-darkBlue" > contact us</Link> for more options or complex requests.</CardDescription>
+				</CardHeader>
 
-					<CardContent className="mb-5">
-						<CustomOrderForm discs={discs} handleDyeChange={handleDyeChange} />
-					</CardContent>
+				<CardContent className="mb-5">
+					<CustomOrderForm discs={discs} handleDyeChange={handleDyeChange} />
+				</CardContent>
+			</Card>
 
-				</Card>
-			</div>
+			{/* contact */}
+			<Card className="p-5 md:col-start-2">
+				<CardHeader>
+					<CardTitle className="text-lg">Looking for something really special, have a big idea, or just have some questions about the process?
+					</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Link href={'/contact'}>
+						<Button variant={"outline"}>
+							Contact Us
+							<AnimatedDiv variant={'outline'} animation={'rotate'}>
+								<Send />
+							</AnimatedDiv>
+						</Button>
+					</Link>
+				</CardContent>
+				<CardFooter className="mt-5">
+					<p>or Email us at <a href="mailto:info@floatondyes.com" className="font-semibold text-darkGreen hover:text-darkBlue"> info@floatondyes.com</a></p>
+				</CardFooter>
+
+			</Card>
 		</section>
 	)
 }
