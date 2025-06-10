e26738805d6a8be252ab9ae828e940e9cb782565
Tish <tishthegoat@gmail.com>
2025-06-10 05:45:55 -0600

    gallery banner instruction

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -4,7 +4,13 @@ import GalleryDisc from "@/components/product/GalleryDisc"
 const GalleryPage = () => {
 	return (
 		<main className="space-y-10">
-			<Banner title="Disc" subtitle="Gallery" url="/images/cellHeader.jpg" />
+			<Banner title="Disc" subtitle="Gallery" url="/images/cellHeader.jpg">
+				<div className="absolute right-1 top-16 mt-1 p-1 text-xs font-extralight text-primary backdrop-blur-xl md:right-5 md:top-14 md:text-base md:font-normal lg:top-20" >
+
+					<p className="text-pretty">Click disc to zoom in</p>
+					<p className="text-pretty">If 'Available', click to go to product page.</p>
+				</div>
+			</Banner>
 			<div className="flex flex-wrap gap-1">
 
 				<GalleryDisc
