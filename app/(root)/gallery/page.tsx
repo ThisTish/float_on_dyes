4a75cad9772a59faa3f84c2ed10f7f9fdf66cd88
Tish <tishthegoat@gmail.com>
2025-07-05 03:48:50 -0600

    gallery page tweaks-title change and availablity specifier

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,47 +1,50 @@
 import Banner from "@/components/header/Banner"
 import GalleryDisc from "@/components/product/GalleryDisc"
 
+// todo get discs that have been dyed and map over instead of hardcoded
+// todo spread them evenly when final pictures are in. x gap too wide
+
 const GalleryPage = () => {
 	return (
-		<main className="space-y-10">
-			<Banner title="Disc" subtitle="Gallery" url="/images/cellHeader.jpg">
-				<div className="absolute right-1 top-16 mt-1 p-1 text-xs font-extralight text-primary backdrop-blur-xl md:right-5 md:top-14 md:text-base md:font-normal lg:top-20" >
+		<main className="space-y-20">
+			<Banner title="Disc" subtitle="Gallery" url="/images/cellHeader.jpg" />
 
-					<p className="text-pretty">Click disc to zoom in</p>
-					<p className="text-pretty">If 'Available', click to go to product page.</p>
-				</div>
-			</Banner>
-			<div className="flex flex-wrap gap-1">
+			<div className="flex flex-wrap space-x-1 space-y-3">
 
 				<GalleryDisc
 					image="/images/discs/cloudbreaker.png"
 					isAvailable={true}
 					dyeType="Cell pull"
 					slug="cloud-breaker-cell-swirl"
+					discName="Cloud Breaker"
 				/>
 				<GalleryDisc
 					image="/images/discs/cloudbreaker.png"
 					isAvailable={false}
 					dyeType="Cell pull"
 					slug="cloud-breaker-cell-swirl"
+					discName="Cloud Breaker"
 				/>
 				<GalleryDisc
 					image="/images/discs/cloudbreaker.png"
 					isAvailable={true}
 					dyeType="Cell pull"
 					slug="cloud-breaker-cell-swirl"
+					discName="Cloud Breaker"
 				/>
 				<GalleryDisc
 					image="/images/discs/cloudbreaker.png"
 					isAvailable={true}
 					dyeType="Cell pull"
 					slug="cloud-breaker-cell-swirl"
+					discName="Cloud Breaker"
 				/>
 				<GalleryDisc
 					image="/images/discs/cloudbreaker.png"
 					isAvailable={true}
 					dyeType="Cell pull"
 					slug="cloud-breaker-cell-swirl"
+					discName="Cloud Breaker"
 				/>
 			</div>
 		</main>
--- components/product/GalleryDisc.tsx
+++ components/product/GalleryDisc.tsx
@@ -1,53 +1,88 @@
 import Image from "next/image"
 import Tooltip from "../ui/Tooltip"
-import { X } from "lucide-react"
+import { CircleOff, X } from "lucide-react"
 import Link from "next/link"
 import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from "../ui/dialog"
+import { BiSolidStore } from "react-icons/bi"
 
 type GalleryDisc = {
 	image: string
+	discName: string
 	isAvailable: boolean
 	dyeType: string
-	slug: string
+	slug?: string
 }
 
-const GalleryDisc = ({ image, isAvailable, dyeType, slug }: GalleryDisc) => {
+// todo button for only show available
+// todo lazy load more images -infinite scroll
+
+const GalleryDisc = ({ image, isAvailable, dyeType, slug, discName }: GalleryDisc) => {
 
 	return (
-		<div className="relative flex flex-col">
+		<div className="group relative flex flex-col">
 
-			<Dialog>
-				<DialogTrigger>
-					<Tooltip label={dyeType}>
-						<Image
-							key={image}
+			{!isAvailable ? (
+				<Dialog>
+					<DialogTrigger>
+						<Tooltip label={dyeType}>
+							<div className="flex flex-col items-center">
+								<Image
+									key={image}
+									src={image}
+									width={300}
+									height={300}
+									alt={`Image of ${dyeType}`}
+									className="hover:cursor-zoom-in"
+								/>
+								<span className="text-center text-sm font-light">{discName}</span>
+							</div>
+						</Tooltip>
+					</DialogTrigger>
+					<DialogContent aria-description="Zoomed in image" className="m-0 flex h-screen max-h-none w-screen max-w-none items-center justify-center border-none p-0">
+						<DialogClose className="absolute right-4 top-4 z-50 text-white opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+							<X className="size-8 text-primary" /><span className="sr-only">Close</span>
+						</DialogClose>
+
+						<DialogTitle className="sr-only" >Zoomed in image of {slug}</DialogTitle>
+						<img
+							className="max-h-full max-w-full object-contain"
 							src={image}
-							width={300}
-							height={300}
-							alt={`Image of ${dyeType}`}
-						className="hover:cursor-zoom-in"
+							alt={`Image of ${slug}`}
 						/>
-					</Tooltip>
-				</DialogTrigger>
-				<DialogContent aria-description="Zoomed in image" className="m-0 flex h-screen max-h-none w-screen max-w-none items-center justify-center border-none p-0">
-					<DialogClose className="absolute right-4 top-4 z-50 text-white opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
-						<X className="size-8 text-primary" /><span className="sr-only">Close</span>
-					</DialogClose>
+					</DialogContent>
+				</Dialog>
+			) : (
+				<Tooltip label={dyeType}>
+					<div className="flex flex-col text-center">
+						<Link href={`/products/${slug}`}>
+							<Image
+								key={image}
+								src={image}
+								width={300}
+								height={300}
+								alt={`Image of ${dyeType}`}
+							/>
+							<span className="text-sm font-light" >{discName}</span>
+						</Link>
+					</div>
+				</Tooltip>
+			)}
 
-					<DialogTitle className="sr-only" >Zoomed in image of {slug}</DialogTitle>
-					<img
-						className="max-h-full max-w-full object-contain"
-						src={image}
-						alt={`Image of ${slug}`}
-					/>
-				</DialogContent>
-			</Dialog>
+			<div className="absolute right-1 top-1 z-10 p-3 md:hidden lg:group-hover:flex">
+				{isAvailable ? (
+					<Link href={`/products/${slug}`}>
+						<BiSolidStore className="text-darkGreen" size={16} />
+					</Link>
+				) : (
+					<CircleOff className="text-lightCta" size={16} />
+				)}
+			</div>
 
-			<span className={`text-center text-sm font-light ${isAvailable ? 'text-lightGreen hover:font-bold' : 'text-lightCta'}`}>
+			{/* <span className={`text-center text-sm font-light ${isAvailable ? 'text-lightGreen hover:font-bold' : 'text-lightCta'}`}>
 				{isAvailable ? (
 					<Link href={`/products/${slug}`}>Available</Link>
 				) : 'Sold'}
-			</span>
+			</span> */}
 		</div>
 	)
 }
