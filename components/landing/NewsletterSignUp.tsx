a8de7e6431a5a7ec57804bd112d7ad844b65a9b7
Tish <tishthegoat@gmail.com>
2025-07-05 02:03:39 -0600

    newsletter style tweaks

--- components/landing/NewsletterSignUp.tsx
+++ components/landing/NewsletterSignUp.tsx
@@ -4,19 +4,21 @@ import { Button } from "../ui/button"
 import Heading from "../ui/Heading"
 import { Input } from "../ui/input"
 
+// todo add to newsletter list functionality
+
 const NewsletterSignUp = () => {
 	return (
 		<section className="mt-32 flex h-fit flex-col space-y-5 bg-darkBlue p-3 py-5 text-white">
 			<Heading first="letter" second="News" className="flex flex-row-reverse justify-center gap-1" />
 			<div className="flex h-fit w-full flex-col justify-around gap-5 p-5">
-				<p className="text-lg font-light md:text-xl">
-					Sign up to get updates on new dyes, discs, and exclusive offers.
+				<p className="text-lg font-light md:w-1/2 md:text-xl">
+					Sign up for our newsletter to be the first to know about new dyes, dyable discs added to our stock, and exclusive deals.
 				</p>
 				<form className="flex flex-col items-center gap-3 self-end md:flex-row">
 					<Input
 						type="email"
 						placeholder="Enter your email"
-						className="w-56 md:w-64"
+						className="h-10 w-56 md:w-64"
 					/>
 					<Button
 						type="submit"
